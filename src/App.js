b5390aab16994453b1ef5fa101274b6635876ebf
iraholdshtein <iragold80@ukr.net>
2021-03-31 18:21:34 +0300

    Change files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+    
+  function increment() {
+    setCounter(counter + 1)
+}
+
+  function decrement() {
+    setCounter(counter - 1) 
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section >
+        <h1>HELLO! lET'S TRY COUNTING {counter}</h1>
+        <button onClick={increment}>" + " </button>
+        <button onClick={decrement}>" - "</button> 
+    </section>
+  )
 }
 
 export default App;
